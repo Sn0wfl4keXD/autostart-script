e283fc9d7b0c2af72ac0ce0851c4125d28c9eb3f
Sn0wFl4ke <dominik.woelfl@live.de>
2020-05-02 22:19:17 +0200

    Create RegisterScript.sh

---
+++ RegisterScript.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+SCRIPT_DIR=/etc/init.d/
+SCRIPT=$1
+CASE=$2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FILE=$DIR"/"$SCRIPT
+
+#puts the script in autorun
+function copy() {
+        echo "Stored file $SCRIPT in $SCRIPT_DIR "
+        sudo cp $FILE $SCRIPT_DIR 
+}
+
+#Delets the script
+function delete(){
+	echo "Deleting $SCRIPT in $SCRIPT_DIR"
+	sudo rm $SCRIPT_DIR$SCRIPT 
+}
+
+function main(){
+	case $CASE in
+		"start")
+		copy
+		;;
+		"stop")
+		delete
+		;;
+		*)
+		echo "Wrong Argument use start/stop"
+		;;
+		esac
+}
+
+
+if [ "$(file --mime-type -b "$FILE")" == "text/x-shellscript" ]
+then
+    main
+else
+    echo "Not a Script"
+fi
